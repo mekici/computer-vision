f8c66b7d6ed6e2d586546d877bec95320b2d9260
Murat Ekici <muratekici83@gmail.com>
2019-04-29 22:09:09 +0300

    computer vision

---
+++ barcode-QRcodeScanner/barcode-QRcodeScanner.py
@@ -0,0 +1,50 @@
+from __future__ import print_function
+import matplotlib.pyplot as plt
+import pyzbar.pyzbar as pyzbar
+import numpy as np
+import cv2
+
+def decode(im) :
+  # Find barcodes and QR codes
+  decodedObjects = pyzbar.decode(im)
+
+  # Print results
+  for obj in decodedObjects:
+    print('Type : ', obj.type)
+    print('Data : ', str(obj.data),'\n')
+
+  return decodedObjects
+
+
+# Display barcode and QR code location
+def display(im, decodedObjects):
+
+  # Loop over all decoded objects
+  for decodedObject in decodedObjects:
+    points = decodedObject.polygon
+    
+    print(points)
+
+    # If the points do not form a quad, find convex hull
+    if len(points) > 4 :
+      hull = cv2.convexHull(np.array([point for point in points], dtype=np.float32))
+      hull = list(map(tuple, np.squeeze(hull)))
+    else :
+      hull = points;
+
+    # Number of points in the convex hull
+    n = len(hull)
+
+    # Draw the convext hull
+    for j in range(0,n):
+      cv2.line(im, hull[j], hull[ (j+1) % n], (255,0,0), 3)
+
+  # Display results
+  plt.imshow(im)
+
+
+# Read image
+im = cv2.imread('zbar-test.jpg')
+
+decodedObjects = decode(im)
+display(im, decodedObjects)
